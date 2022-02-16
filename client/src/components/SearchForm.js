be67b055068d6d23524e766bc3802835610cbc9e
gerlinp <gerlin.pierrelouis@gmail.com>
2022-02-16 17:18:12 -0500

    taking it back

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,36 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Form, Button } from "react-bootstrap"
-import Exams from "../data/exams.json";
-import PropTypes from 'prop-types';
-=======
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
->>>>>>> parent of 9ab68c7 (Merge branch 'main' of https://github.com/gerlinp/swe-techDive-solo into main)
 
-export const SearchForm = ({handleOnChange, str}) => {
-  console.log(str)
-  return (
+class AddPlayerForm extends Component {
 
-<<<<<<< HEAD
-  <div className="dash-form">
-    <Form className='center'>
-      <input 
-        type="text"
-        placeholder="SEARCH Patient ID"
-        name="searchStr"
-        onChange={handleOnChange}
-        value={str}
-      />
-      <Button className="add-button">
-        Add Exam +
-      </Button>  
-    </Form>
-    <div className="search-info text-center">Total Exams: {Exams.length} </div>
-    </div>
-  
-  );
-=======
   static propTypes = {
     addPlayer: PropTypes.func
   };
@@ -58,10 +30,6 @@ export const SearchForm = ({handleOnChange, str}) => {
       </form>
     );
   }
->>>>>>> parent of 9ab68c7 (Merge branch 'main' of https://github.com/gerlinp/swe-techDive-solo into main)
 }
-
-SearchForm.propTypes = {
-  handleOnChange: PropTypes.func.isRequired,
-  str: PropTypes.string.isRequired
-}
+ 
+export default AddPlayerForm;
