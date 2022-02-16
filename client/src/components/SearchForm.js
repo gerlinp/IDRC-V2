ef2e2f32ffbb7c680f5b941e956ce72479606e3c
Amanda Marcal <amandabmarcal@gmail.com>
2022-02-16 15:43:40 -0500

    reverting change

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,12 +1,18 @@
+<<<<<<< HEAD
 import React from 'react'
 import { Form, Button } from "react-bootstrap"
 import Exams from "../data/exams.json";
 import PropTypes from 'prop-types';
+=======
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+>>>>>>> parent of 9ab68c7 (Merge branch 'main' of https://github.com/gerlinp/swe-techDive-solo into main)
 
 export const SearchForm = ({handleOnChange, str}) => {
   console.log(str)
   return (
 
+<<<<<<< HEAD
   <div className="dash-form">
     <Form className='center'>
       <input 
@@ -24,6 +30,35 @@ export const SearchForm = ({handleOnChange, str}) => {
     </div>
   
   );
+=======
+  static propTypes = {
+    addPlayer: PropTypes.func
+  };
+
+  playerInput = React.createRef();
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addPlayer(this.playerInput.current.value);
+    e.currentTarget.reset();
+  }
+
+  render() {
+    return (
+      <form className='center' onSubmit={this.handleSubmit}>
+        <input 
+          type="text"
+          ref={this.playerInput}
+          placeholder="Enter Exam info"
+        />
+        <input 
+          type="submit"
+          value="Search"
+        />
+      </form>
+    );
+  }
+>>>>>>> parent of 9ab68c7 (Merge branch 'main' of https://github.com/gerlinp/swe-techDive-solo into main)
 }
 
 SearchForm.propTypes = {
